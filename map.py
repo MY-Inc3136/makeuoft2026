43376b89c6ff94c076bad3d9b6f2a1f59359cc5f
Meil C <meilc@example.com>
2026-02-15 10:41:24 -0500

    map saves multiple markers

--- map.py
+++ map.py
@@ -3,9 +3,9 @@ from PIL import Image, ImageDraw
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-def add_marker(x, y):
+def add_marker(x, y, index):
     try:
-        with Image.open("blank_map.png") as img:
+        with Image.open(f"map{index}.png") as img:
             img = img.convert("RGBA")
             draw = ImageDraw.Draw(img)
             
@@ -15,14 +15,20 @@ def add_marker(x, y):
             # Coordinates are (left, top, right, bottom)
             draw.ellipse((x-r, y-r, x+r, y+r), fill=(255, 0, 0, 180), outline="white")
             
-            img.save("updated_map.png")
+            img.save(f"map{index+1}.png")
             print(f"Marker placed at: {x}, {y}")
             
     except Exception as e:
         print(f"Error: {e}")
 
 print("Listening for coordinates...")
+
+loopCount = 0
+
 while True:
+    if loopCount >= 10: # creates up to 10 markers
+        break
+
     if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').strip()
         
@@ -32,6 +38,7 @@ while True:
             raw_coords = line.replace("MAP:", "")
             try:
                 x_str, y_str = raw_coords.split(",")
-                add_marker(int(x_str), int(y_str))
+                add_marker(int(x_str), int(y_str), loopCount)
+                loopCount+=1
             except ValueError:
                 print("Received messy data, skipping...")
